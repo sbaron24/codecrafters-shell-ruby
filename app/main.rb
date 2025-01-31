5698e1ce4f6ee84aedd2c8478250f2c5e9c46da9
Sean B <xseany@gmail.com>
2025-01-31 17:29:58 -0500

    echo builtin

--- app/main.rb
+++ app/main.rb
@@ -6,6 +6,9 @@ loop do
     command, *args = gets.chomp.split(" ")
     if command == 'exit' && args.first == '0'
         break
+    elsif command == 'echo'
+        $stdout.write(args.join(" "))
+        $stdout.write("\n")
     elsif command
         $stdout.write("#{command}: command not found")
         $stdout.write("\n")
